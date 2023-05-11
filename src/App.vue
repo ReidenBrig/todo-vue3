2057a7079e36e4294a1ac194975c91eb68d2bc4a
flustern <coolgrell@mail.ru>
2023-05-11 22:48:55 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1 class="title">
+      {{ title }}
+    </h1>
+
+    <div class="todo">
+      <input v-model="todo" type="text" placeholder="type todo..."/>
+      <button @click="addTodo">Add todo</button>
+
+      <to-do-list :todos="todos" @removeTodo="removeTodo" @isDoneTodo="isDoneTodo"/>
+
+      <hr>
+      <p>
+        Список задач: {{ todos.length }}
+      </p>
+
+    </div>
+
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import ToDoList from "@/components/ToDoList";
+import {v4 as uuidv4} from 'uuid';
+
 
 export default {
-  name: 'App',
+
+
+
+  data() {
+    return {
+      title: 'ToDo app',
+      todo: '',
+      todos: [],
+      isComplete: false,
+      id: null,
+    };
+  },
+  mounted() {
+    const data = localStorage.getItem('todos');
+    data ? this.todos = JSON.parse(data) : null;
+  },
+  methods: {
+    addTodo() {
+      if (this.todo != '') {
+        this.todos.push({
+          id: uuidv4(),
+          text: this.todo,
+          isComplete: false,
+        })
+
+        localStorage.setItem('todos', JSON.stringify(this.todos))
+      }
+
+      this.todo = '';
+    },
+    isDoneTodo() {
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+    ToDoList,
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +83,51 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.title {
+  text-align: center;
+}
+
+.todo {
+  margin: 0 auto;
+  padding: 30px;
+  width: 500px;
+  height: 100%;
+  background-color: #dcd9d9;
+  //outline: 1px solid black;
+  border-radius: 10px;
+  min-height: 500px;
+
+  &__box {
+    margin-top: 10px;
+    min-width: 250px;
+    border: #2c3e50 1px solid;
+    border-radius: 10px;
+  }
+
+  &__id {
+    color: #2c3e50;
+  }
+
+  &__text {
+    font-size: 15px;
+    color: #2c3e50;
+    text-transform: capitalize;
+
+    &_isShow {
+      color: grey;
+      text-decoration: line-through;
+    }
+  }
+
+  &__delete {
+    padding: 0 5px;
+    border: 1px solid #2c3e50;
+    border-radius: 3px;
+    margin-right: 10px;
+    float: right;
+  }
+
+}
+
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="todo__box" v-for="(todo,i) in todos" :key="todo.id">
+    <p>
+      <span class="todo__id">{{ i + 1 }}.</span>
+      <span
+          class="todo__text"
+          :class="{todo__text_isShow: todo.isComplete}">
+            {{ todo.text }}
+          </span>
+      <input @change="isDoneItem" v-model="todo.isComplete" type="checkbox"/>
+      <span class="todo__delete" @click="removeItem(i)">x</span>
+    </p>
+
+  </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+export default defineComponent({
+
+  props: ['todos', 'removeTodo', 'isDoneTodo'],
+  setup() {
+    function removeItem(index) {
+      this.$emit('removeTodo', index)
+    }
+    function isDoneItem() {
+      this.$emit('isDoneTodo')
+
+    }
+    return {
+      removeItem,
+      isDoneItem
+    }
+
+
+  },
+/*  methods: {
+    removeItem(index) {
+      this.$emit('removeTodo', index)
+    },
+    isDoneItem() {
+      this.$emit('isDoneTodo')
+
+    }
+  }*/
+})
+</script>
+
+<style scoped>
+
+</style>
